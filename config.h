aa5438adc317bfa93d2bcdd75a0fb60289ec4c70
Mikael Regnell <mikael.regnell@redpill-linpro.com>
2020-12-04 16:34:23 +0100

    Added swallow patch

--- config.h
+++ config.h
@@ -6,7 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 1;        /* 0 means no systray */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
@@ -36,9 +37,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class              instance    title           tags mask     isfloating   isterminal noswallow   monitor */
+	{ "Gimp",             NULL,       NULL,           0,            1,           0,         0,        -1 },
+	{ "Firefox",          NULL,       NULL,           1 << 8,       0,           0,        -1,        -1 },
+	{ "Gnome-terminal",   NULL,       NULL,           0,            0,           1,         0,        -1 },
+	{ "St",               NULL,       NULL,           0,            0,           1,         0,        -1 },
+	{ NULL,               NULL,       "Event Tester", 0,            0,           0,         1,        -1 },
 };
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
